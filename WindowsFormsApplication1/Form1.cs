1a83172e8d52c98f48aaf9a2b2f82b9eef024244
Petrik Lajos Szg <info@petrik.hu>
2018-01-17 15:46:11 +0100

    harmadik commit , kitorli a dolgokat , ellenorzi ha baj van

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-       
+      
         public Form1()
         {
             InitializeComponent();
@@ -26,10 +26,18 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
            List<Hiba> hibak = new List<Hiba>();
-           
+            if (!String.IsNullOrEmpty(textBox1.Text) && !String.IsNullOrEmpty(textBox2.Text) && !String.IsNullOrEmpty(richTextBox1.Text))
+            {
                 hibak.Add(new Hiba(textBox1.Text, textBox2.Text, richTextBox1.Text));
-                
-       
+                textBox1.Text = null;
+                textBox2.Text =null;
+                richTextBox1.Text =null;
+            }
+            else {
+                MessageBox.Show("ajjajj ures valami mezo",
+"valamelyik mezovel baj van ajj ajj");
+            }
+            
         }
 
     }
